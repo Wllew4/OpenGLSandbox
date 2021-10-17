0c3e40e7115373929e667b7540fcfe76df4e52c5
Wllew4 <willjanelle2@gmail.com>
2021-10-16 20:51:19 -0400

    now using premake vscode template

--- premake5.lua
+++ premake5.lua
@@ -1,15 +1,16 @@
-workspace "WorkspaceName"
-    configurations { "Debug", "Release" }
+workspace "OpenGLSandbox"
+    configurations { "Release" }
 
-project "ProjectName"
+project "Sandbox"
     --Compiler/Linker Options
     kind "ConsoleApp"
     language "C++"
     cppdialect "C++17"
+    linkoptions { '/NODEFAULTLIB:"LIBCMT"' }
     
     --Files/Directories
     targetdir "bin"
-    targetname "out"
+    targetname "sandbox"
     files
     {
         "src/**.h",
@@ -17,22 +18,23 @@ project "ProjectName"
     }
     includedirs
     {
-        "src"
+        "src",
+        "lib/glfw/include",
+        "lib/glew/include"
     }
     libdirs
     {
-        
+        "lib/glfw/lib-vc2019",
+        "lib/glew/Win32"
     }
     links
     {
-        
+        "glfw3",
+        "glew32s",
+        "opengl32"
     }
 
     --Configurations
     filter "configurations:Release"
         defines { "RELEASE" }
-        symbols "Off"
-
-    filter "configurations:Debug"
-        defines { "DEBUG" }
         symbols "Off"
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 
-int main(int argc, char** argv)
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    if(!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return 1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "o/ from GLFW", NULL, NULL);
+    if (!window) {
+        fprintf(stderr, "ERROR: could not open window with GLFW3\n");
+        glfwTerminate();
+        return 1;
+    }
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
