096ac5238e6669198b1bcbbea73781d1974856b5
Wllew4 <willjanelle2@gmail.com>
2021-10-16 23:17:14 -0400

    hello triangle!

--- src/main.cpp
+++ src/main.cpp
@@ -25,9 +25,59 @@ int main()
 
     std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
 
+    float triangle[] = 
+    {
+        0.0f,   0.5f,   0.0f,
+        0.5f,   -0.5,   0.0f,
+        -0.5f,  -0.5f,  0.0f
+    };
+
+    GLuint vbo = 0;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), triangle, GL_STATIC_DRAW);
+
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+    const char* vert_s =
+        "#version 400\n"
+        "in vec3 vp;"
+        "void main() {"
+        "   gl_Position = vec4(vp, 1.0);"
+        "}";
+
+    const char* frag_s =
+        "#version 400\n"
+        "out vec4 frag_color;"
+        "void main() {"
+        "   frag_color = vec4(0.5, 0.0, 0.5, 1.0);"
+        "}";
+
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vert_s, NULL);
+    glCompileShader(vs);
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &frag_s, NULL);
+    glCompileShader(fs);
+
+    GLuint shader_prog = glCreateProgram();
+    glAttachShader(shader_prog, vs);
+    glAttachShader(shader_prog, fs);
+    glLinkProgram(shader_prog);
+
     while(!glfwWindowShouldClose(window))
     {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(shader_prog);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         glfwPollEvents();
+        glfwSwapBuffers(window);
     }
 
     glfwTerminate();
